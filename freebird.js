994d55a3c338d35ce6a780e41c3bed3d29e1356b
info@s0s.app <info@s0s.app>
2020-06-01 10:39:01 +0000

    Added freebird to delete messages after a set interval, reduces logging

---
+++ freebird.js
@@ -0,0 +1,36 @@
+var Botkit = require('botkit')
+
+// load bot secret file
+const bot_secret = require('./lib/bot-secret')
+
+// load common bot functions
+var app_name = bot_secret.discord_server_name
+var bot = require('./lib/bot')
+var freebird = new bot()
+
+var delete_after = 5000
+
+//setup connection to discord
+var discord = require('discord.js')
+var discord_bot = new discord.Client()
+discord_bot.login(bot_secret.freebird_secret_token)
+
+// setup connection to the database
+var chan_bluebird
+discord_bot.on('ready', () => {})
+
+discord_bot.on('message', (receivedMessage) => {
+  // copy all messages to the bluebird channel
+  // except messages in the bluebird channel
+  var msg_chan = receivedMessage.channel.name
+  if (msg_chan == app_name) {
+  } else {
+    if (receivedMessage.author != discord_bot.user) {
+      // limit to numbered channels
+      if (msg_chan.match(/^\d+/)) {
+        var msg = receivedMessage.id
+        receivedMessage.delete(delete_after)
+      }
+    }
+  }
+})
